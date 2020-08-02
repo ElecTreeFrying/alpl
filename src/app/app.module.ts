c8fc07a1f8bf025cc842b75db4f88e72f91d50dd
John James G. Ermitaño <jjgermitano@gmail.com>
2020-08-02 17:46:27 +0800

    added working base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppRoutingModule } from './app-routing.module';
+import { AppProviderModule } from './_common/providers/app-provider.module';
+
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    AppProviderModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,52 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  hotelCurrenciesAndPriceCompetitivenessData: {
+    data: 'https://5df9cc6ce9f79e0014b6b3dc.mockapi.io/hotels/tokyo',
+    prices: {
+      usd: {
+        url: 'http://5df9cc6ce9f79e0014b6b3dc.mockapi.io/hotels/tokyo/1/USD',
+        country: 'United States of America',
+        currency: {
+          name: 'dollar',
+          short: 'usd',
+          long: 'U.S. Dollars',
+          symbol: '$'
+        }
+      },
+      sgd: {
+        url: 'http://5df9cc6ce9f79e0014b6b3dc.mockapi.io/hotels/tokyo/1/SGD',
+        country: 'Singapore',
+        currency: {
+          name: 'dollar',
+          short: 'sgd',
+          long: 'Singapore Dollars',
+          symbol: '$'
+        }
+      }, 
+      cny: {
+        url: 'http://5df9cc6ce9f79e0014b6b3dc.mockapi.io/hotels/tokyo/1/CNY',
+        country: 'China',
+        currency: {
+          name: 'Yuan',
+          short: 'cny',
+          long: 'Chinese Yuan',
+          symbol: '¥'
+        }
+      },
+      krw: {
+        url: 'http://5df9cc6ce9f79e0014b6b3dc.mockapi.io/hotels/tokyo/1/KRW',
+        country: 'South Korea',
+        currency: {
+          name: 'won',
+          short: 'krw',
+          long: 'South Korean Won',
+          symbol: '₩'
+        }
+      }
+    }
+  }
 };
 
 /*
